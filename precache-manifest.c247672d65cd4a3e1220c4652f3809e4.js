02ed709a137c3335a17f3547bd52926aa35db6c8
Adarsha C R <adarsha@cloudthing.com>
2020-04-11 20:22:54 +0530

    Updates

---
+++ precache-manifest.c247672d65cd4a3e1220c4652f3809e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bfb310854704b5c0a04ffb85023b45",
+    "url": "/covid19logger/index.html"
+  },
+  {
+    "revision": "2fc273a51410aa0fc2ef",
+    "url": "/covid19logger/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a144bd168bc44ef04f05",
+    "url": "/covid19logger/static/css/main.d62113b6.chunk.css"
+  },
+  {
+    "revision": "2fc273a51410aa0fc2ef",
+    "url": "/covid19logger/static/js/2.00e25b93.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid19logger/static/js/2.00e25b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a144bd168bc44ef04f05",
+    "url": "/covid19logger/static/js/main.a42b8746.chunk.js"
+  },
+  {
+    "revision": "63ce3759fb1f7fd6aac1",
+    "url": "/covid19logger/static/js/runtime-main.ef87170a.js"
+  }
+]);
